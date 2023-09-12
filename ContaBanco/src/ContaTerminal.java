266362f4e15652d37872d9105729c0aa727e348a
karinapapa <karinaolivcosta@gmail.com>
2023-09-12 19:20:22 -0300

    pacote atualizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Informe seu nome: ");
+        String nomeCliente = ler.next();
+
+        System.out.print("Informe sua agência: ");
+        String agencia = ler.next();
+
+        System.out.print("Informe seu saldo: ");
+        float saldo = ler.nextFloat();
+
+        System.out.print("Informe número da sua conta: ");
+        int numeroConta = ler.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponivel para saque");
     }
 }
